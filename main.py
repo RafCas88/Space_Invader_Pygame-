a6f594a0e3d9df166203e2684e204fc9bd94f70f
RafCas88 <raffaelecastaldo88@libero.it>
2020-04-27 17:04:16 +0100

    player displayed

--- main.py
+++ main.py
@@ -11,9 +11,29 @@ pygame.display.set_caption('Space Invaders')
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+#player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 #game loop
 running = True
 while running:
+
+    #RGB red, green , blue
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+
+
+    player()
+    pygame.display.update()
