b0fd4214ced0c1b566ec10df0a5cdd2e93dc664d
RafCas88 <raffaelecastaldo88@libero.it>
2020-04-27 16:13:23 +0100

    icon added

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800, 600))
 
+#title and icon
+pygame.display.set_caption('Space Invaders')
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
 #game loop
 running = True
 while running:
