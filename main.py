3c0737407224240d5740bced66bf37b5c9711fcb
RafCas88 <raffaelecastaldo88@libero.it>
2020-04-28 12:00:32 +0100

    going to add background img

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize the pygame
 pygame.init()
@@ -17,9 +18,21 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+#enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.9
+enemyY_change = 40
+
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 #game loop
@@ -35,13 +48,31 @@ while running:
     #if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -0.9
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 0.9
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    #checking for boundaries of spaceship so it does not go out of bounds
     playerX += playerX_change
+
+    if playerX <=0:
+        playerX = 0
+    elif playerX >=736:
+        playerX = 736
+
+    #enemy movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.9
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.9
+        enemyY += enemyY_change
+
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
