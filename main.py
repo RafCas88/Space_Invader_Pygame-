34f86af404784dcdfe7b4ec54177845ab43463a4
RafCas88 <raffaelecastaldo88@libero.it>
2020-04-28 10:51:37 +0100

    speed added to spaceship

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 #game loop
@@ -27,13 +28,20 @@ while running:
 
     #RGB red, green , blue
     screen.fill((0, 0, 0))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    #if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
-
-
-    player()
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
